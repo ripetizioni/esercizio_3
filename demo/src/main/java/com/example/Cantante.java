18fcc6fbfe235962920078c4dc9cf12ddcad7e6c
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-21 11:05:27 +0100

    da fare la logica della classe cantante

--- demo/src/main/java/com/example/Cantante.java
+++ demo/src/main/java/com/example/Cantante.java
@@ -6,7 +6,46 @@ public class Cantante {
     private String codiceFiscale;
 
     public Cantante (String nome, String cognome, String codiceFiscale){
-        this.nome=
+        this.nome=nome;
+        this.cognome=cognome;
+        this.codiceFiscale=codiceFiscale;
+    }
+    
+    public Cantante(Cantante c){
+        this.nome=c.getNome();
+        this.cognome=c.getCognome();
+        this.codiceFiscale=c.getCodiceFiscale();
+    }
+
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    } 
+    
+    @Override
+    public String toString(){
+        return "\nnome: "+nome  +"\ncognome: " +  cognome +"\ncodiceFiscale: " + codiceFiscale;
     }
     
 }
--- demo/src/main/java/com/example/Canzone.java
+++ demo/src/main/java/com/example/Canzone.java
@@ -1,5 +1,66 @@
 package com.example;
 
+//visibilità tipo_variabile_ritorno nomeFunzione(tipo_variabile nome_parametro, ...)
+
 public class Canzone {
+    private String codiceCanzone;
+    private Cantante cantante;
+    private double durata;
+    private int annoDiUscita;
+    private double prezzo;
     
+    public Canzone(String codiceCanzone,Cantante cantante,double durata, int annoDiUscita,double  prezzo){
+        this.codiceCanzone=codiceCanzone;
+        this.cantante=cantante;
+        this.durata=durata;
+        this.annoDiUscita=annoDiUscita;
+        this.prezzo=prezzo;
+    }
+
+    public String getCodiceCanzone() {
+        return codiceCanzone;
+    }
+
+    public void setCodiceCanzone(String codiceCanzone) {
+        this.codiceCanzone = codiceCanzone;
+    }
+
+    public Cantante getCantante() {
+        return cantante;
+    }
+
+    public void setCantante(Cantante cantante) {
+        this.cantante = cantante;
+    }
+
+    public double getDurata() {
+        return durata;
+    }
+
+    public void setDurata(double durata) {
+        this.durata = durata;
+    }
+
+    public int getAnnoDiUscita() {
+        return annoDiUscita;
+    }
+
+    public void setAnnoDiUscita(int annoDiUscita) {
+        this.annoDiUscita = annoDiUscita;
+    }
+
+    public double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(double prezzo) {
+        this.prezzo = prezzo;
+    }
+    
+    @Override
+    public String toString(){
+        return "\nIl codice della canzone e': "+ codiceCanzone + "\nIl cantante e' : "+ cantante.toString() + "\nLa canzone dura: : " + durata + "\nE' uscita nell'anno: ; " + annoDiUscita + "\nIl costo e' : " + prezzo;
+    }
+
 }
+
