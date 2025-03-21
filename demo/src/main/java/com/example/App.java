a47be6c6ef67f80d96782a12a2a77bacf966b19c
Diego <diego.wartbuechler0@gmail.com>
2025-03-21 11:11:49 +0100

    prova push

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+        System.out.println();
         System.out.println( "Hello World!" );
-    }
+    }           
+
+
+
+
+    
 }
