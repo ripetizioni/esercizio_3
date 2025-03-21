9a0b3079b2c25121034605ef5945155a0f96d29c
macchina_kali <iacopoliberolavoro@gmail.com>
2025-03-21 10:34:47 +0100

    init

---
+++ demo/src/main/java/com/example/Cantante.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Cantante {
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+
+    public Cantante (String nome, String cognome, String codiceFiscale){
+        this.nome=
+    }
+    
+}
